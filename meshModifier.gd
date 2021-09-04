f605d62805ba4d50bc4454c0cf36f3e4844eb467
darth negative hunter <73083466+thenegativehunter2@users.noreply.github.com>
2021-09-04 12:28:54 +0430

    api changes

--- meshModifier.gd
+++ meshModifier.gd
@@ -18,7 +18,7 @@ func resetSurfaces():
 		if(i < surfaceColors.size()):
 			var color : Color = Color(surfaceColors[i],0,0,0)
 			for j in range(dataTool.get_vertex_count()):
-				dataTool.set_vertex_custom(j, 0, color)
+				dataTool.set_vertex_custom_float(j, 0, color)
 				# the color can be read from "CUSTOM0".
 				# values are not clamped so no worries there.
 		dataTools.append(dataTool)
